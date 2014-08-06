2733cda7bd8a90ac9ee86b52557f10f3d461d864
Ralph Tice <ralph.tice@gmail.com>
2014-08-06 01:08:02 -0500

    all the things

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-shadowing
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/clojure_shadowing/core.clj
@@ -0,0 +1,9 @@
+(ns clojure-shadowing.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
+(defn report [] "i blow things up by shadowing")
